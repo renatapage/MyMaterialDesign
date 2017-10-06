1ef6e44ef7323f2610b76de26b8a65e0d84932ca
Renata Page <renatafx@gmail.com>
2017-10-05 21:39:03 -0300

    Final da aula 10, mas fiz merdinha na pasta drawable.

--- app/src/main/java/br/com/renatapage/mymaterialdesign/StandaloneToolbar.java
+++ app/src/main/java/br/com/renatapage/mymaterialdesign/StandaloneToolbar.java
@@ -3,6 +3,7 @@ package br.com.renatapage.mymaterialdesign;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 /**
  * Created by renatapage on 03/10/17.
@@ -15,6 +16,10 @@ public class StandaloneToolbar extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toolbar);
 
+
+        Toolbar mToolbar = (Toolbar)findViewById(R.id.toolbar);
+        mToolbar.setTitle("Standalone Toolbar !");
+        mToolbar.setSubtitle("by Renata !");
 
     }
 }
